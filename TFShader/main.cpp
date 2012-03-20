59446e92c96a887cc2db684ac173da7b495472b0
Jorge Herrera <jherreras@gmail.com>
2012-03-19 23:51:00 -0700

    Initial Commit

---
+++ TFShader/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TFShader
+//
+//  Created by Jorge Herrera on 3/19/12.
+//  Copyright (c) 2012 Stanford. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
